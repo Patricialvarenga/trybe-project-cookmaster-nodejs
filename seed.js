32f79d22288f93ed46c49f0f0e13da033da8d4e1
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2021-12-01 17:58:50 -0300

    requisito 6 com aspas, agora ok

--- seed.js
+++ seed.js
@@ -1,2 +1,1 @@
-// colocar query do MongoD
-db.users.insertOne({name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin'});
+"db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });"
